2e66e6ebbb594b21c664857ad6f4bbd324b8188a
mason <masonandwang@gmail.com>
2022-10-27 21:03:37 +1030

    tutorial 2

---
+++ tutorial2/index.js
@@ -0,0 +1,27 @@
+// BMI = kg/m2
+
+// const weight = 100;
+// const height = 1.8;
+
+// const weight = prompt('Please enter your weight in kg');
+// const height = prompt('Please enter your height in m');
+
+// const BMI = weight / (height * height);
+
+// console.log(`Your BMI is ${BMI}`);
+
+let a = [1];
+let b = a;
+
+// a.push(2)
+
+a = [6, 66];
+
+console.log(b);
+
+const 优点 = ['帅', '高'];
+
+const 我 = {
+  家庭住址: 'xxx',
+  优点: 优点,
+};
